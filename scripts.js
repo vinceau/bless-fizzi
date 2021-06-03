ba60ff5c1626cd12981c3e57141ffb6c509d7a08
Vince Au <vinceau09@gmail.com>
2021-06-03 10:41:30 +1000

    feat: add year support

--- scripts.js
+++ scripts.js
@@ -5,10 +5,12 @@ function getTimeRemaining(endtime) {
   const seconds = Math.floor((total / 1000) % 60);
   const minutes = Math.floor((total / 1000 / 60) % 60);
   const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+  const days = Math.floor((total / (1000 * 60 * 60 * 24)) % 365);
+  const years = Math.floor(total / (1000 * 60 * 60 * 24 * 365));
 
   return {
     total,
+    years,
     days,
     hours,
     minutes,
@@ -18,8 +20,8 @@ function getTimeRemaining(endtime) {
 
 function updateCountdown() {
   const result = getTimeRemaining(announcementTimestamp);
-  const { days, hours, minutes, seconds } = result;
-  const format = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+  const { years, days, hours, minutes, seconds } = result;
+  const format = `${years} years, ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
   document.querySelector("#countdown").innerHTML = format;
 }
 
